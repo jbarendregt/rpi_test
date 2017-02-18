07809560984017ddf8c11a6b876496031e39ed9d
Joost Barendregt <mail@joostbarendregt.com>
2017-02-18 10:48:15 +0100

    adios

--- test_buttons.py
+++ test_buttons.py
@@ -6,6 +6,11 @@ button = Button(14)
 def say_hello():
     print("Hello!")
 
+def say_adios():
+    print("!Adios!")
+
+
 button.when_pressed = say_hello
+button.when_released = say_adios
 
 pause()
